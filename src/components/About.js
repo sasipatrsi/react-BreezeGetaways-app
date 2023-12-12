134431f92829815f32a9cfc1de4a966ca5ef8c68
sasipatrsi <sasipatrsi.myprojects@gmail.com>
2023-12-12 20:36:14 +0700

    add new picture

--- src/components/About.js
+++ src/components/About.js
@@ -1,31 +1,27 @@
 import React from 'react'
-import aboutImg from '../images/about.jpeg'
+import aboutImg from '../images/about2.jpeg'
 import Title from './Title'
 
 const About = () => {
   return (
     <section className="section" id="about">
-      <Title title='about' subTitle='us' />
+      <Title title="about" subTitle="us" />
 
       <div className="section-center about-center">
         <div className="about-img">
-          <img
-            src={aboutImg}
-            className="about-photo"
-            alt="awesome beach"
-          />
+          <img src={aboutImg} className="about-photo" alt="awesome beach" />
         </div>
         <article className="about-info">
           <h2>explore the difference</h2>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
-            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
-            unde dolor?
+            Welcome to BreezeGetaways, Your Gateway to Unforgettable Adventures!
           </p>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
-            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
-            unde dolor?
+            At BreezeGetaways, we believe that travel is not just about reaching
+            a destination; it's about the experiences, the memories, and the
+            moments that create a lifetime of stories. Established with a
+            passion for exploration, BreezeGetaways is your trusted partner in
+            crafting extraordinary journeys that go beyond the ordinary.
           </p>
           <a href="#" className="btn">
             read more
