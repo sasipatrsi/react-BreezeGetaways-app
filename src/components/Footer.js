5808f8700e07bbc02e71b56e7a1837ed37728b24
sasipatrsi <sasipatrsi.myprojects@gmail.com>
2023-12-18 14:31:25 +0700

    add all new pictures, tour5 and tour6

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
           return (
             <li key={id}>
               <a href={href} className="footer-link">
-               {text}
+                {text}
               </a>
             </li>
           )
@@ -18,24 +18,19 @@ const Footer = () => {
       </ul>
 
       <ul className="footer-icons">
-        {socialLinks.map((link)=>{
-          const { id, href, icon } = link;
+        {socialLinks.map((link) => {
+          const { id, href, icon } = link
           return (
             <li key={id}>
-              <a
-                href={href}
-                target="_blank"
-                className="footer-icon"
-              >
+              <a href={href} target="_blank" className="footer-icon">
                 <i className={icon}></i>
               </a>
             </li>
           )
         })}
-        
       </ul>
       <p className="copyright">
-        copyright &copy; Backroads travel tours company
+        copyright &copy; BreezeGetaways travel tours company
         <span id="date">{new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
